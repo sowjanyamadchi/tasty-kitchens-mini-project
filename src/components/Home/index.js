e16256292405ec8320b7ecba302a628a73c26b98
sowjanyamadchi <sowjanya.madchi1@gmail.com>
2022-10-31 08:08:59 +0000

    tastykitchens

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+
+import RestaurantSection from '../RestaurantSection'
+import Footer from '../Footer'
+
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <RestaurantSection />
+    <Footer />
+  </>
+)
+
+export default Home
